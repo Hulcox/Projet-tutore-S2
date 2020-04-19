691a126997cb96c9e0c7ee8fd5132d618951dad8
Simon Bredeche <simon.bredeche@u-psud.fr>
2020-04-19 21:34:28 +0200

    added dependencies to the new map

---
+++ map/Paysage.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="Paysage" tilewidth="32" tileheight="32" tilecount="30" columns="10">
+ <image source="Paysage.png" width="320" height="96"/>
+</tileset>
