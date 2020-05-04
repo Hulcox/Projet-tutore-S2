37f99c900b9626dc4583e27a79b12687ae6a41ca
Simon Bredeche <simon.bredeche@u-psud.fr>
2020-05-04 14:36:39 +0200

    added animation fix

--- src/lesson1/AnimationsAsset.java
+++ src/lesson1/AnimationsAsset.java
@@ -54,7 +54,7 @@ public class AnimationsAsset {
     	this.setSpellAnimation(spriteSheetMaelStrom, 5, imageasset.MaelStrom);
     	this.setSpellAnimation(spriteSheetMegaStorm, 8, imageasset.MegaStorm);
     	//Chest animation
-    	this.setChestAnimation(spriteSheetChest, 12, imageasset.chest1);
+    	this.setChestAnimation(spriteSheetChest, 12, imageasset.chest1, imageasset);
     }
     public void setEnemyAnimation(SpriteSheet spriteSheet, int numberOfFrame, Enemie enemie) {
     	Animation animations[] = new Animation[2];
@@ -68,12 +68,13 @@ public class AnimationsAsset {
         animations[0] = loadAnimation(spriteSheet, 1, numberOfFrame, 0);
     	spell.setAnimation(animations);
     }
-    public void setChestAnimation(SpriteSheet spriteSheet, int numberOfFrame, Chest chest) {
+    public void setChestAnimation(SpriteSheet spriteSheet, int numberOfFrame, Chest chest, GameAsset imageasset) {
     	Animation animations[] = new Animation[3];
         animations[0] = loadAnimation(spriteSheet, 0, 1, 0);
         animations[1] = loadAnimation(spriteSheet, 1, numberOfFrame, 0);
         animations[2] = loadAnimation(spriteSheet, numberOfFrame-1, numberOfFrame, 0);
         chest.setAnimations(animations);
+        imageasset.chest2.setAnimations(animations);
     }
 
 
