cff92c63dd066034045419d3a4bf9f7a7cdfb78e
Simon Bredeche <simon.bredeche@u-psud.fr>
2020-05-31 22:18:46 +0200

    added assets to finish game

---
+++ map/eglisetile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="eglisetile" tilewidth="32" tileheight="32" tilecount="165" columns="11">
+ <image source="eglise1.png" width="352" height="480"/>
+</tileset>
