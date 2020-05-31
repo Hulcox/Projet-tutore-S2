ad92b4a1f97aa0eb7b6535e583a88015ffd4e578
Bidault Romain <romainbidault08@gmail.com>
2020-05-31 10:46:10 +0200

    fixed player baseDammage HUD background

--- src/lesson1/HUD.java
+++ src/lesson1/HUD.java
@@ -21,6 +21,7 @@ public class HUD {
     private static final int LIFE_BAR_Y = 20 + P_BAR_Y;
     private static final int E_LIFE_BAR_Y = 20 + E_BAR_Y; //position de la barre de vie des enemies
     private static final int MANA_BAR_Y = 40 + P_BAR_Y;
+    private static final int BACKGROUND_BAR_Y = 20 + E_BAR_Y;
     private static final int XP_BAR_Y = 60 + P_BAR_Y;
     private static final int BAR_WIDTH = 206;
     private static final int BAR_HEIGHT = 15;
@@ -28,6 +29,7 @@ public class HUD {
     private static final Color LIFE_COLOR = new Color(255, 0, 0);
     private static final Color MANA_COLOR = new Color(0, 0, 255);
     private static final Color XP_COLOR = new Color(0, 255, 0);
+    private static final Color BACKGROUND_COLOR = new Color(10,28,68);
     private String chemin;
 
     HUD(String chemin, int P_BAR_X, int P_BAR_Y, Player player){
@@ -43,6 +45,8 @@ public class HUD {
 
     public void renderPlayer(Graphics g) {
         g.resetTransform();
+        g.setColor(BACKGROUND_COLOR);
+        g.fillRect(BAR_X, BACKGROUND_BAR_Y,1*BAR_WIDTH,60);
         g.setColor(LIFE_COLOR);
         g.fillRect(BAR_X, LIFE_BAR_Y, ((float)player.getPv()/(float)player.getMaxPV()) * BAR_WIDTH, BAR_HEIGHT);
         g.setColor(MANA_COLOR);
@@ -65,6 +69,8 @@ public class HUD {
 
     public void renderEnemie(Graphics g) {
         g.resetTransform();
+        g.setColor(BACKGROUND_COLOR);
+        g.fillRect(EBAR_X, BACKGROUND_BAR_Y,1*BAR_WIDTH,20);
         g.setColor(LIFE_COLOR);
         g.fillRect(EBAR_X, E_LIFE_BAR_Y, ((float)enemie.getPv()/(float)enemie.getMaxHp()) * BAR_WIDTH, BAR_HEIGHT);
         g.drawImage(this.hud_enemie, E_BAR_X, E_BAR_Y);
